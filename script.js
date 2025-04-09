f1a37226428e4c3c15798a2bcd9f0fa071ec8416
Kamal <lawalkamal595@gmail.com>
2025-04-09 21:22:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = "Dazai"; //String
+const age = 60; //Number
+let is_approved = false // Boolean
+let level = undefined; //Undefined
+let space = null //Null
+let user = {
+    name:"Dazai",
+    age:60
+}; //Object
+
+console.log(name)
+console.log(age)
+console.log(is_approved)
+console.log(level)
+console.log(space)
+console.log(user)
+
+console.log("My name is " + name + " I will be " + age + " this year."  )
